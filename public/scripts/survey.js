8821731d021d4873d9d36973992fb01e7d07717a
Robert J. Paruch <roobbb@live.com>
2019-04-24 03:08:26 -0700

    Added code for survey.js

---
+++ public/scripts/survey.js
@@ -0,0 +1,60 @@
+const questions = [
+  'Red or Blue?',
+  'Sweet or Sour',
+  'Sea or Mountains',
+  'Sunny or Cloudy',
+  'Mild or Spicy',
+  'Walk or Ride',
+  'Horror or Drama',
+  'Tea or Coffee',
+  'Shrimp or Steak',
+  'Beer or Margarita'
+];
+
+$(window).on('load', () =>
+  questions.forEach((question, index) =>
+    $('#questions').append(
+      `<div class="form-group">
+         <label for="question-${index}">${question}</label>
+         <input type="range" class="form-control-range" id="question-${index}" min="0" max="100" value="50" step="1">
+     </div>`
+    )
+  )
+);
+
+$('#submit-button').click((event) => {
+  event.preventDefault();
+
+  const nickname = $('#nickname').val().trim();
+  const photoLink = $('#photo-link').val().trim();
+
+  if (!nickname) {
+    $('#nickname-label').text('Provide Your Nickname').css('color', 'red');
+    alert('Provide your nickname!');
+    return;
+  }
+  if (!photoLink) {
+    $('#photo-link-label').text('Provide Link to Your Photo').css('color', 'red');
+    alert('Provide link to your photo!');
+    return;
+  }
+
+  const results = [];
+
+  $('.form-control-range').each(function() { // arrow function doesn't work here because it doesn't bind `this`
+    results.push($(this).val());
+  });
+
+  const newFriend = {
+    nickname: nickname,
+    photoLink: photoLink,
+    results: results
+  };
+
+  $.post("/api/friends", newFriend, (bestFriend) => {
+    $('#result-photo').empty().append(`<img src="${bestFriend.photoLink}" alt="${bestFriend.nickname}">`);
+    $('#result-nickname').empty().append(`<h3>${bestFriend.nickname}</h3>`);
+    $('#results-modal').modal('show');
+  });
+
+});
