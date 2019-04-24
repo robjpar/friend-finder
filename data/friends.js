e9d4499dbc19d3586dd67b9b3659b2c6c5fa7ec8
Robert J. Paruch <roobbb@live.com>
2019-04-24 03:07:59 -0700

    Added sample data

---
+++ data/friends.js
@@ -0,0 +1,21 @@
+module.exports = [{
+  "nickname": "Jay Dog",
+  "photoLink": "http://lorempixel.com/output/animals-q-c-300-200-8.jpg",
+  "results": ["18", "71", "28", "84", "76", "55", "74", "66", "18", "76"]
+}, {
+  "nickname": "Rachel Giraffe",
+  "photoLink": "http://lorempixel.com/output/animals-q-c-300-200-4.jpg",
+  "results": ["96", "73", "14", "12", "50", "3", "30", "12", "1", "78"]
+}, {
+  "nickname": "Phoenix Tiger",
+  "photoLink": "http://lorempixel.com/output/animals-q-c-300-200-3.jpg",
+  "results": ["91", "24", "69", "64", "12", "16", "38", "45", "82", "48"]
+}, {
+  "nickname": "Melissa Deer",
+  "photoLink": "http://lorempixel.com/output/animals-q-c-300-200-5.jpg",
+  "results": ["66", "84", "3", "86", "24", "22", "52", "65", "77", "74"]
+}, {
+  "nickname": "Lark Gorilla",
+  "photoLink": "http://lorempixel.com/output/animals-q-c-300-200-2.jpg",
+  "results": ["80", "54", "10", "22", "25", "44", "92", "29", "8", "48"]
+}];
